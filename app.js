32c27ad249a2360c5afea50f9b0dd666814a38dd
davidm5 <dmcca@protonmail.com>
2017-05-30 14:57:13 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ window.addEventListener('load', function() {
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
-    window.web3 = new(web3.currentProvider);
+    window.web3 = web3.currentProvider;
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
