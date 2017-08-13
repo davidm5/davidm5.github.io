646d7bdd118b5992612e697e236acd0171c54be8
davidm5 <dmcca@protonmail.com>
2017-08-13 11:46:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -114,7 +114,9 @@ window.addEventListener('load', function() {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     //  Our fallback is to use a local node.
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    ////window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/52bYG6b7WXeqNBhfWcFH"));
+
   }
 
   App.start();
